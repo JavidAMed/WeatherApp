24f7ba50427d1cd5252c77211ab2fe566f06550d
JavidAMed <123823644+JavidAMed@users.noreply.github.com>
2024-02-29 15:14:58 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+const APIkey="66b5db0a90e8c29c7054f160d85972a2";
+const APIurl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const srchBox= document.querySelector('.search-box input');
+const srchBtn= document.querySelector('.search-box button');
+const weatherIcon= document.querySelector('.weather-icon');
+
+
+async function checkWeather(City){
+    const response= await fetch(APIurl + City +`&appid=${APIkey}`);
+    let data= await response.json();
+
+    if(response.status === 404){
+        document.querySelector('.error').style.display= 'block';
+    }
+
+    else{
+
+    document.querySelector('.City').innerHTML= data.name;
+    document.querySelector('.temp').innerHTML= `${Math.round(data.main.temp)}°C`;
+    document.querySelector('.humidity-number').innerHTML= `${Math.round(data.main.humidity)}%`;
+    document.querySelector('.windspeed-number').innerHTML= `${Math.round(data.wind.speed)} km/hr`;
+    
+
+    if (data.weather[0].main == 'Clouds'){
+        weatherIcon.src = './weather-app-img/images/clouds.png';
+    }
+
+    else if (data.weather[0].main == 'Clear'){
+        weatherIcon.src = './weather-app-img/images/clear.png';
+    }
+
+    else if (data.weather[0].main == 'Rain'){
+        weatherIcon.src = './weather-app-img/images/rain.png';
+    }
+
+    else if (data.weather[0].main == 'Drizzle'){
+        weatherIcon.src = './weather-app-img/images/drizzle.png';
+    }
+
+    else if (data.weather[0].main == 'Snow'){
+        weatherIcon.src = './weather-app-img/images/snow.png';
+    }
+
+    else if (data.weather[0].main == 'Wind'){
+        weatherIcon.src = './weather-app-img/images/wind.png';
+    }
+
+    else if (data.weather[0].main == 'Mist'){
+        weatherIcon.src = './weather-app-img/images/mist.png';
+    }
+
+    document.querySelector('.content').style.display= 'block';
+    document.querySelector('.error').style.display= 'none';
+
+    console.log(data);
+}
+
+}
+srchBtn.addEventListener("click",()=>{checkWeather(srchBox.value)});
